fe5b6f4582bf54ebb87c1b8e9820f8c645794d6c
LayneHansen <layne.d.hansen@gmail.com>
2020-10-11 14:52:06 -0600

    fixed seed.sql file, now working in workbench

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ inquirer
     .prompt ([
     {
         type: "list",
-        name: "view",
+        name: "action",
         message: "What would you like to do?",
         choices: 
             [
