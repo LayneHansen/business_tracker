06103a7875c7557764d69f8f51717a1428a74977
LayneHansen <layne.d.hansen@gmail.com>
2020-10-11 21:41:44 -0600

    updated seed.sql data, added functions and switch statements in businessTrack function

--- index.js
+++ index.js
@@ -1,30 +1,170 @@
 // require files
 
-const fs = require("fs");
+const mysql = require("mysql");
 const inquirer = require("inquirer");
-const consoleTable = require("console.table");
+const env = require("dotenv").config();
+// const consoleTable = require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.password,
+    database: "business_db"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    businessTrack();
+  });
 
 // inquirer function
-    // .prompt
 
-inquirer    
-    .prompt ([
-    {
-        type: "list",
-        name: "action",
-        message: "What would you like to do?",
-        choices: 
-            [
-                "View Department",
-                "Add Department",
-                "View Employee",
-                "Add Employee",
-                "Update Employee",
-                "View Roles",
-                "Add Role"
-            ]
-    }    
-    ]);
+function businessTrack() {
+    inquirer    
+        .prompt ({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: 
+                [
+                    "View Department",
+                    "Add Department",
+                    "View Employee",
+                    "Add Employee",
+                    "Update Employee",
+                    "View Roles",
+                    "Add Role"
+                ] 
+        })
+        .then (function(response) {
+            switch (response.action) {
+            case "View Department":
+                viewDepartment();
+                break;
+
+            case "Add Department":
+                addDepartment();
+                break;
+            
+            case "View Employee":
+                viewEmployee();
+                break;
+            
+            case "Add Employee":
+                addEmployee();
+                break;
+            
+            case "Update Employee":
+                updateEmployee();
+                break;
+            
+            case "View Role":
+                addDepartment();
+                break;
+            
+            case "Add Role":
+                addRole();
+                break;
+            }
+        })
+}    
+    // function for when department doesn't exist
+    // function to see current departments
+    
+    function viewDepartment() {
+        inquirer    
+            .prompt ({
+                name: "viewDepartment",
+                type: "list"
+                choices: 
+                    [
+                        ""
+                    ]
+            })
+        }
+    
+    function addDepartment() {
+        inquirer    
+            .prompt ({
+                name: "addDepartment",
+                type: "list"
+                choices: 
+                    [
+                        ""
+                    ]
+            })
+        }
+    
+    function viewEmployee() {
+        inquirer    
+            .prompt ({
+                name: "viewEmployee",
+                type: "list"
+                choices: 
+                    [
+                        ""
+                    ]
+            })
+        }
+    
+    function addEmployee() {
+        inquirer    
+            .prompt ({
+                name: "addEmployee",
+                type: "list"
+                choices: 
+                    [
+                        ""
+                    ]
+            })
+        }
+    
+    function addDepartment() {
+        inquirer    
+            .prompt ({
+                name: "addDepartment",
+                type: "list"
+                choices: 
+                    [
+                        ""
+                    ]
+            })
+        }
+    
+    function updateEmployee() {
+        inquirer    
+            .prompt ({
+                name: "updateEmployee",
+                type: "list"
+                choices: 
+                    [
+                        ""
+                    ]
+            })
+        }
+    
+    function addRole() {
+        inquirer    
+            .prompt ({
+                name: "addRole",
+                type: "list"
+                choices: 
+                    [
+                        ""
+                    ]
+            })
+        }
+
+
+    
+    // function to add department
+    // function for when employees don't exist
+    // function to add employee
+    // function to update employee
+    // function for when roles don't exist
+    // function to add role
+
 
 
     // .then
--- seed.sql
+++ seed.sql
@@ -33,11 +33,40 @@ CREATE TABLE employee (
     
     );
 
-INSERT INTO department (name)
-VALUES ("Human Resources");
+INSERT INTO department (name) VALUES ("Human Resources");
+INSERT INTO department (name) VALUES ("Billing");
+INSERT INTO department (name) VALUES ("Shipping");
+INSERT INTO department (name) VALUES ("Receiving");
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Director", 75000, 1);
+INSERT INTO role (title, salary, department_id) 
+VALUES ("Manager", 75000, 1),
+("Supervisor", 60000, 1),
+("Associate", 45000, 1),
+("Manager", 75000, 2),
+("Supervisor", 60000, 2),
+("Associate", 45000, 2),
+("Manager", 75000, 3),
+("Supervisor", 60000, 3),
+("Associate", 45000, 3),
+("Engineer", 75000, 3),
+("Manager", 75000, 4),
+("Supervisor", 60000, 4),
+("Associate", 45000, 4),
+("Engineer", 75000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id, department_id)
-VALUES ("Mike", "Jones", 1, 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Jones", 1, 1), 
+("Bill", "Smith", 2, 1),
+("Cleon", "Salmon", 3, 1),
+("Marge", "Gundersen", 1, 2),
+("Barry", "Badrinath", 2, 2),
+("Moose", "Skowron", 3, 2),
+("Billy", "Batts", 1, 3),
+("Getu", "Thachoppa", 2, 3),
+("Sally", "Strothers", 3, 3),
+("Milly", "VanGogh", 4, 3),
+("Willy", "Umerrymee", 1, 4),
+("Barrien", "Bigmyke", 2, 4),
+("Donald", "Bigly", 3, 4),
+("Hillary", "Killery", 3, 4),
+("Kummon", "Gitdaune", 4, 4)
