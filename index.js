6721b40f4d08ddf1f28c375592fdd414609c2bb2
LayneHansen <layne.d.hansen@gmail.com>
2020-10-12 18:20:32 -0600

    complete query to create new employee

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const env = require("dotenv").config();
-// const consoleTable = require("console.table");
+const consoleTable = require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,12 +15,12 @@ var connection = mysql.createConnection({
   
   connection.connect(function(err) {
     if (err) throw err;
-    businessTrack();
+    runSearch();
   });
 
 // inquirer function
 
-function businessTrack() {
+function runSearch() {
     inquirer    
         .prompt ({
             name: "action",
@@ -37,8 +37,8 @@ function businessTrack() {
                     "Add Role"
                 ] 
         })
-        .then (function(response) {
-            switch (response.action) {
+        .then (function(data) {
+            switch (data.action) {
             case "View Department":
                 viewDepartment();
                 break;
@@ -74,57 +74,112 @@ function businessTrack() {
     
     function viewDepartment() {
         inquirer    
-            .prompt ({
-                name: "viewDepartment",
-                type: "list"
-                choices: 
-                    [
-                        ""
-                    ]
-            })
-        }
+            .prompt ([
+                {
+                    name: "viewDept",
+                    type: "list",
+                    message: "Which department would you like to view?",
+                    choices: 
+                        [
+                            "Human Resources",
+                            "Billing",
+                            "Shipping",
+                            "Receiving"
+                        ]
+                },
+                    
+            ])  
+            .then(function(data) {
+                var query = "SELECT * FROM department";
+                connection.query(query, {name: data.viewDept}, function (err, res) {
+                    // for (var i = 0; i < res.length; i++) {
+                    // console.log("ID: " + res[i].id + " Name: " + res[i].name);
+                    console.table("ID: " + "Name: ");
+                    })
+                    runSearch();
+            });
+    };
+            
     
     function addDepartment() {
         inquirer    
-            .prompt ({
-                name: "addDepartment",
-                type: "list"
-                choices: 
-                    [
-                        ""
-                    ]
-            })
-        }
+        .prompt ({
+            name: "addDepartment",
+            type: "input",
+            message: "What is the name of the new department?"
+        })
+        .then(function(data) {
+            connection.query("INSERT INTO deparment WHERE ?",)
+
+        })
+    }
     
     function viewEmployee() {
-        inquirer    
-            .prompt ({
-                name: "viewEmployee",
-                type: "list"
-                choices: 
-                    [
-                        ""
-                    ]
+        var query = "SELECT * FROM employee INNER JOIN department ON employee.id = department.id INNER JOIN role ON department.id = role.id";
+        connection.query(query, function (err, res) {
+            if (err) throw err;
+            // for (var i = 0; i < res.length; i++) {
+            // console.log("ID: " + res[i].id + " Name: " + res[i].name);
+            console.table(res);
             })
-        }
-    
+        runSearch();
+
+    }
+        
     function addEmployee() {
         inquirer    
-            .prompt ({
-                name: "addEmployee",
-                type: "list"
-                choices: 
-                    [
-                        ""
-                    ]
-            })
+            .prompt ([
+                {
+                name: "first_name",
+                type: "input",
+                message: "What is the employee's first name?"
+                },
+                {
+                name: "last_name",
+                type: "input",
+                message: "What is the employee's last name?"
+                },
+                {
+                name: "role_id",
+                type: "input",
+                message: "What is the employee's role? 1) Manager 2) Supervisor 3) Associate 4) Engineer:",
+                // choices: 
+                    // [
+                    //     "Manager",
+                    //     "Supervisor",
+                    //     "Associate",
+                    //     "Engineer"
+                    // ]
+                },
+                {
+                name: "manager_id",
+                type: "input",
+                message: "Who is the employee's manager? 1) Mike Jones 2) Marge Gundersen 3) Billy Batts 4) Willy Umerrymee:",
+                },
+            ])
+            .then(function(data) {
+                // when finished prompting, insert a new item into the db with that info
+                connection.query("INSERT INTO employee SET ?",
+                  {
+                    first_name: data.first_name,
+                    last_name: data.last_name,
+                    role_id: data.role_id,
+                    manager_id: data.manager_id,
+                  },
+                  function(err) {
+                    if (err) throw err;
+                    console.log("The employee was created successfully!");
+                    // re-prompt the user for if they want to bid or post
+                    runSearch();
+                });
+            });
         }
-    
+    /*
     function addDepartment() {
         inquirer    
             .prompt ({
                 name: "addDepartment",
-                type: "list"
+                type: "list",
                 choices: 
                     [
                         ""
@@ -168,3 +223,6 @@ function businessTrack() {
 
 
     // .then
+
+    */
+  
